d997ec1db6fbb01ff5160a24703138823c0a42ad
Tiantian100 <65786457+Tiantian100@users.noreply.github.com>
2024-08-07 01:22:48 -0400

    Add files via upload

---
+++ src/main/java/com/FWRP/dto/NotificationDTO.java
@@ -0,0 +1,104 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.FWRP.dto;
+
+/**
+ *
+ * @author Tiantian
+ */
+public class NotificationDTO {
+    private int id;
+    private Long timestamp;
+    private int type;
+    private int status;
+    private int userid;
+    private int inventoryid;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the timestamp
+     */
+    public Long getTimestamp() {
+        return timestamp;
+    }
+
+    /**
+     * @param timestamp the timestamp to set
+     */
+    public void setTimestamp(Long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    /**
+     * @return the type
+     */
+    public int getType() {
+        return type;
+    }
+
+    /**
+     * @param type the type to set
+     */
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    /**
+     * @return the status
+     */
+    public int getStatus() {
+        return status;
+    }
+
+    /**
+     * @param status the status to set
+     */
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    /**
+     * @return the userid
+     */
+    public int getUserid() {
+        return userid;
+    }
+
+    /**
+     * @param userid the userid to set
+     */
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
+
+    /**
+     * @return the inventoryid
+     */
+    public int getInventoryid() {
+        return inventoryid;
+    }
+
+    /**
+     * @param inventoryid the inventoryid to set
+     */
+    public void setInventoryid(int inventoryid) {
+        this.inventoryid = inventoryid;
+    }
+    
+    
+}
