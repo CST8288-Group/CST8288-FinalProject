4398f027c400dc76b6d709dbcd5b2ba5c17e5acb
Tiantian100 <65786457+Tiantian100@users.noreply.github.com>
2024-08-07 01:34:24 -0400

    Add files via upload

---
+++ src/main/java/com/FWRP/dto/PreferredFoodDTO.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.FWRP.dto;
+
+/**
+ *
+ * @author Tiantian
+ */
+public class PreferredFoodDTO {
+    private int id;
+    private int userid;
+    private int fooditemid;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the userid
+     */
+    public int getUserid() {
+        return userid;
+    }
+
+    /**
+     * @param userid the userid to set
+     */
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
+
+    /**
+     * @return the fooditemid
+     */
+    public int getFooditemid() {
+        return fooditemid;
+    }
+
+    /**
+     * @param fooditemid the fooditemid to set
+     */
+    public void setFooditemid(int fooditemid) {
+        this.fooditemid = fooditemid;
+    }
+    
+    
+}
---
+++ src/main/java/com/FWRP/dto/SubscriptionDTO.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.FWRP.dto;
+
+/**
+ *
+ * @author Tiantian
+ */
+public class SubscriptionDTO {
+    private int id;
+    private int userid;
+    private int locationid;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the userid
+     */
+    public int getUserid() {
+        return userid;
+    }
+
+    /**
+     * @param userid the userid to set
+     */
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
+
+    /**
+     * @return the locationid
+     */
+    public int getLocationid() {
+        return locationid;
+    }
+
+    /**
+     * @param locationid the locationid to set
+     */
+    public void setLocationid(int locationid) {
+        this.locationid = locationid;
+    }
+    
+    
+}
